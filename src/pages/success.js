2329a2b51df609fb9b1adb30f5b767fda86285f7
Norkaz <j.seblad@gmail.com>
2021-12-09 08:46:25 +0100

    Successpage

---
+++ src/pages/success.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Structure from '../components/structure/Structure'
+import Text from '../components/controls/text/Text'
+
+export default function Success() {
+    return (
+        <Structure>
+            <section>
+                <Text>
+                    Succeed.
+                </Text>
+            </section>          
+        </Structure>
+    )
+}
