62626afcf7bd4c949a8a44cc38a7295a2cf74a81
Jseblad <j.seblad@gmail.com>
2021-12-19 23:14:16 +0100

    returned code

--- src/pages/products/product/[id].js
+++ src/pages/products/product/[id].js
@@ -21,5 +21,79 @@ export default function Product(props) {
     product.price * (product.discountPercent / 100)
   )
 
-  return <div>hej</div>
+  return (
+    <>
+      <Helmet>
+        <title>
+          {siteData.name} - {product.name}
+        </title>
+        <meta property="og:type" content="content-page" />
+      </Helmet>
+      <Structure>
+        <Grid gap="20px">
+          <Column type="full">
+            <Img
+              src="https://facegram.se/CDN/13-2.jpg"
+              header={product.name}
+              headertag="h1"
+              color="#FFF"
+              shadow={false}
+            />
+          </Column>
+        </Grid>
+        <section>
+          <Grid gap="20px" type="mobile-full">
+            <Column>
+              <div className="product-image">
+                {savingPrice ? (
+                  <span className="badge">{product.discountPercent + "%"}</span>
+                ) : null}
+                <Img
+                  popup={true}
+                  srcLarge={
+                    "https://facegram.se/CDNproducts/articlenumbers/" +
+                    product.artNr +
+                    "_XL" +
+                    ".jpg"
+                  }
+                  src={
+                    "https://facegram.se/CDNproducts/articlenumbers/" +
+                    product.artNr +
+                    "_L" +
+                    ".jpg"
+                  }
+                  alt={product.name}
+                  width="400px"
+                  height="400px"
+                />
+              </div>
+            </Column>
+            <Column>
+              <h2>{product.intro}</h2>
+              <div className="price-container">
+                {savingPrice ? (
+                  <>
+                    <div className="price discount">
+                      {product.price - savingPrice} {product.currency}
+                    </div>
+                    <div className="regular-price">
+                      {product.price} {product.currency}
+                    </div>
+                  </>
+                ) : (
+                  <div className="price">
+                    {product.price} {product.currency}
+                  </div>
+                )}
+              </div>
+              <p>{product.description}</p>
+            </Column>
+          </Grid>
+        </section>
+        <section>
+          <ShopWindow header="Skyltfönster" />
+        </section>
+      </Structure>
+    </>
+  )
 }
