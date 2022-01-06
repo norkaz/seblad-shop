902a9dfff44eb08abfce65def9078241b5e2f0f4
Norkaz <j.seblad@gmail.com>
2022-01-06 19:26:12 +0100

    Expired OG-Image made STATIC

--- src/data/site/sitedata.js
+++ src/data/site/sitedata.js
@@ -1,5 +1,6 @@
-export const siteData = { 
-    name: "Seblad keramik",
-    description: "Nils Seblad Keramiker, från Spånga, nuvarande boende i Strängnäs",
-    ogImage: "https://scontent-arn2-1.xx.fbcdn.net/v/t31.18172-8/15128937_700753090075065_6281275094518666522_o.jpg?_nc_cat=109&cb=c578a115-7e291d1f&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=JPRDtb6yHVoAX8oYkDa&_nc_ht=scontent-arn2-1.xx&oh=d2bc64300250437836ffdf4e2299e48c&oe=61D5F6D8"
- }
+export const siteData = {
+  name: "Seblad keramik",
+  description:
+    "Nils Seblad Keramiker, från Spånga, nuvarande boende i Strängnäs",
+  ogImage: "https://facegram.se/CDN/seblad_keramik.jpg",
+}
