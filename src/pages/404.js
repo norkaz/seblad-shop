2f6f202e0bbfe400d1ba6f05c6b9febbd0962837
Jseblad <j.seblad@gmail.com>
2022-01-11 23:07:13 +0100

    removed all 404 content

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,5 @@
 import React from "react"
-import Structure from "../components/structure/Structure"
 
 export default function NotFound() {
-  return (
-    <>
-      <Structure>
-        <h1>Sidan kunde inte hittas</h1>
-      </Structure>
-    </>
-  )
+  return null
 }
