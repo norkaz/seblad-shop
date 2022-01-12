29919afd3c29bdaefea228464578abc25d6e8e24
Norkaz <j.seblad@gmail.com>
2022-01-12 09:41:59 +0100

    Revert "removed Grid and Widgets from 404"
    This reverts commit a31f0b9493a7ee44bbf928750b48f2e1389e40ed.

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,6 +2,8 @@ import React from "react"
 import { Helmet } from "react-helmet"
 import { siteData } from "../data/site/sitedata"
 import Structure from "../components/structure/Structure"
+import Grid from "../components/gridLayout/grid/Grid"
+import Widget from "../components/gridLayout/widget/Widget"
 import { Link } from "gatsby"
 
 export default function NotFound() {
@@ -12,13 +14,17 @@ export default function NotFound() {
         <meta property="og:type" content="404" />
       </Helmet>
       <Structure>
-        <div className="text-box">
-          <h1>Hoppsan! Sidan du försökte gå till hittades inte</h1>
-          <p>
-            Gå till <Link to="/products">produkter</Link> eller till{" "}
-            <Link to="/">startsidan</Link>.
-          </p>
-        </div>
+        <Grid>
+          <Widget>
+            <div className="text-box">
+              <h1>Hoppsan! Sidan du försökte gå till hittades inte</h1>
+              <p>
+                Gå till <Link to="/products">produkter</Link> eller till{" "}
+                <Link to="/">startsidan</Link>.
+              </p>
+            </div>
+          </Widget>
+        </Grid>
       </Structure>
     </>
   )
